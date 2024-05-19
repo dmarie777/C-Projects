8b4adf6a3b4edf17317f022bc647e8c887eb97b2
dmarie777 <deimarie777@gmail.com>
2024-05-19 11:00:16 -0500

    Added functionality for the coinFlip

--- CoinFlip/main.cpp
+++ CoinFlip/main.cpp
@@ -2,3 +2,14 @@
 #include <stdlib.h>
 #include <ctime>
 
+int main(){
+    //Create a number that is 0 or 1
+    srand(time(NULL));
+    int coin = rand() % 2;
+
+    if(coin == 0) {
+        std::cout << "Head\n";
+    } else {
+        std::cout << "Tail\n";
+    }
+}
